c6d2c81ea08e3eb47255b36c211fbb4d8851cec9
Nadir Hamid <matrix.nad@gmail.com>
2023-01-17 19:23:44 +0000

    fix some issues for env setup

--- utils/utils.go
+++ utils/utils.go
@@ -334,9 +334,12 @@ Config func to get env value from key ---
 */
 func Config(key string) string {
 	// load .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Print("Error loading .env file")
+	loadDotEnv := os.Getenv("USE_DOTENV")
+	if loadDotEnv != "off"  {
+		err := godotenv.Load(".env")
+		if err != nil {
+			fmt.Print("Error loading .env file")
+		}
 	}
 	return os.Getenv(key)
 }
