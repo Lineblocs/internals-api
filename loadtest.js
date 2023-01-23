4ad3fc68935c06770c88f46e47891cf18ee7b7e0
vadymluzin <vadymluzin111@gmail.com>
2023-01-23 03:18:57 -0500

    #IA-6 Benchmark API Update loadtest script file

--- loadtest.js
+++ loadtest.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Rate } from 'k6/metrics';
 
-export const errorRate = new Rate('non_200_requests');
+export const errorRate = new Rate('non_success_requests');
 
 export let options = {
   stages: [
@@ -18,8 +18,8 @@ export let options = {
   thresholds: {
     // We want the 95th percentile of all HTTP request durations to be less than 500ms
     "http_req_duration": ["p(95)<1000"],
-    // Thresholds based on the custom metric `non_200_requests`.
-    "non_200_requests": [
+    // Thresholds based on the custom metric `non_success_requests`.
+    "non_success_requests": [
       // Global failure rate should be less than 1%.
       "rate<0.01",
       // Abort the test early if it climb over 5%.
@@ -33,11 +33,11 @@ export default function () {
   const baseUrl = "http://localhost:8000";
 
   // Healthz
-
+  /*
   const url = baseUrl + "/healthz";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
-
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
+  */
   // CreateCall
   /*
   const url = baseUrl + "/call/createCall";
@@ -55,11 +55,10 @@ export default function () {
     user_id: 2,
     workspace_id: 2,
     channel_id: "01gmvb228y61ay5b7afh9npxgx-ch",
-    call_id: "1c11ae5423119c115143863e291d2f70@155.138.140.32:5160",
-    comments: uuidv4(),
+    call_id: "1c11ae5423119c115143863e291d2f70@155.138.140.32:5160"
   });
   check(http.post(url,data, params), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -76,7 +75,7 @@ export default function () {
     status: "ended"
   });
   check(http.post(url,data, params), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -84,7 +83,7 @@ export default function () {
   /*
   const url = baseUrl + "/call/fetchCall?id=256";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // SetSIPCallID
@@ -95,7 +94,7 @@ export default function () {
     "apiid": 255
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -107,7 +106,7 @@ export default function () {
     "apiid": 2572
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -205,7 +204,7 @@ export default function () {
     "domain": "workspace.com"
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -219,7 +218,7 @@ export default function () {
     "name": "test"
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -241,7 +240,7 @@ export default function () {
     tags: ["a", "b", "c"]
   });
   check(http.post(url,data,params), {
-    'status is 200': (r) => r.status == 200,
+    'status was 200': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -253,7 +252,7 @@ export default function () {
     "recording_id": 5
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -271,7 +270,7 @@ export default function () {
     text: "test2"
   });
   check(http.post(url,data,params), {
-    'status is 200': (r) => r.status == 200,
+    'status was 200': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -279,7 +278,7 @@ export default function () {
   /*
   const url = baseUrl + "/recording/getRecording?id=4";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // CreateSIPReport
@@ -290,7 +289,7 @@ export default function () {
     "status": 200
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -298,177 +297,176 @@ export default function () {
   /*
   const url = baseUrl + "/carrier/processRouterFlow?callto=15874874526&callfrom=17808503688&userid=2";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // VerifyCaller
   /*
   const url = baseUrl + "/user/verifyCaller?workspace_id=2&number=15874874526";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // VerifyCallerByDomain
   /*
   const url = baseUrl + "/user/verifyCallerByDomain?domain=workspace&number=17808503688";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetUserByDomain
   /*
   const url = baseUrl + "/user/getUserByDomain?domain=workspace";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetUserByDID
   /*
   const url = baseUrl + "/user/getUserByDID?did=23";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetUserByTrunkSourceIp
   /*
   const url = baseUrl + "/user/getUserByTrunkSourceIp?source_ip=155.138.144.230";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetWorkspaceMacros
   /*
   const url = baseUrl + "/user/getWorkspaceMacros?workspace=2";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetDIDNumberData
   /*
   const url = baseUrl + "/user/getDIDNumberData?number=15874874526";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
   
   // GetPSTNProviderIP
   /*
   const url = baseUrl + "/user/getPSTNProviderIP?from=15874874526&to=17808503688&domain=workspace";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetPSTNProviderIPForTrunk
   /*
   const url = baseUrl + "/user/getPSTNProviderIPForTrunk?from=15874874526&to=17808503688";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // IpWhitelistLookup
   /*
   const url = baseUrl + "/user/ipWhitelistLookup?ip=toronto.voip.ms&domain=workspace";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetDIDAcceptOption
   /*
   const url = baseUrl + "/user/getDIDAcceptOption?did=23";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetDIDAssignedIP
   /*
   const url = baseUrl + "/user/getDIDAssignedIP";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetUserAssignedIP
   /*
   const url = baseUrl + "/user/getUserAssignedIP?rtcOptimized=true&domain=workspace&routerip=155.138.140.32";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetTrunkAssignedIP
   /*
   const url = baseUrl + "/user/getTrunkAssignedIP";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // AddPSTNProviderTechPrefix
   /*
   const url = baseUrl + "/user/addPSTNProviderTechPrefix";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetCallerIdToUse
   /*
   const url = baseUrl + "/user/getCallerIdToUse?domain=workspace&extension=3000";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetExtensionFlowInfo
   /*
   const url = baseUrl + "/user/getExtensionFlowInfo?extension=3000&workspace=2";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetFlowInfo
   /*
-  const url = baseUrl + "/user/getFlowInfo?flow_id=2&workspace=2";
+  const url = baseUrl + "/user/getFlowInfo?flow_id=f-b8320fa4-68b2-4f5b-8a4a-dbcff9afb047&workspace=2";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetDIDDomain
   /*
   const url = baseUrl + "/user/getDIDDomain";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // GetCodeFlowInfo
   /*
   const url = baseUrl + "/user/getCodeFlowInfo?code=2&workspace=2";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // IncomingDIDValidation
   /*
   const url = baseUrl + "/user/incomingDIDValidation?did=23&number=15874874526&source=158.85.70.148";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // IncomingTrunkValidation
   /*
   const url = baseUrl + "/user/incomingTrunkValidation?fromdomain=155.138.144.230";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // LookupSIPTrunkByDID
   /*
   const url = baseUrl + "/user/lookupSIPTrunkByDID?did=23";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // IncomingMediaServerValidation
   /*
   const url = baseUrl + "/user/incomingMediaServerValidation?source=155.138.140.32";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
-
 
   // StoreRegistration
   /*
@@ -478,7 +476,7 @@ export default function () {
     "user": 3000
   };
   check(http.post(url,data), {
-    'status is 200': (r) => r.status == 200,
+    'status was success': (r) => r.status >= 200 && r.status <= 226,
   }) || errorRate.add(1);
   */
 
@@ -486,14 +484,14 @@ export default function () {
   /*
   const url = baseUrl + "/user/getSettings";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // ProcessSIPTrunkCall
   /*
   const url = baseUrl + "/user/processSIPTrunkCall?did=23";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   // SendAdminEmail
@@ -516,7 +514,7 @@ export default function () {
   /*
   const url = baseUrl + "/getBestRTPProxy";
   let res = http.get(url);
-  check(res, { "status was 200": (r) => r.status == 200 }) || errorRate.add(1);
+  check(res, { "status was success": (r) => r.status >= 200 && r.status <= 226}) || errorRate.add(1);
   */
 
   sleep(Math.random()*1+1); //Random sleep betweeen 1s and 2s.
