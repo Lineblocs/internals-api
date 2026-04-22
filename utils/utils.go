92badc64cdbaf0015f3fb2a3d00dc61a8cdc4ce5
Nadir Hamid <matrix.nad@gmail.com>
2026-04-22 01:41:45 +0000

    change extension matching regex.

--- utils/utils.go
+++ utils/utils.go
@@ -366,7 +366,7 @@ func IsOverageEnabled(customizations *helpers.CustomizationSettingsKV) bool {
 }
 
 func IsExtensionToExtensionCall(from string, number string) bool {
-	// Assuming extension numbers are purely numeric and 4-6 digits long
-	matched, _ := regexp.MatchString(`^\d{4,6}$`, number)
+	// Assuming extension numbers are purely numeric and 1-6 digits long
+	matched, _ := regexp.MatchString(`^\d{1,6}$`, number)
 	return matched
 }
