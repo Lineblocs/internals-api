679970f00f55e7f43c464473cb15dd6161711bcd
Nadir Hamid <matrix.nad@gmail.com>
2026-04-22 01:29:25 +0000

    do not use extension to extension call matching in verify caller API

--- utils/utils.go
+++ utils/utils.go
@@ -364,3 +364,9 @@ func IsOverageEnabled(customizations *helpers.CustomizationSettingsKV) bool {
 
 	return allowed
 }
+
+func IsExtensionToExtensionCall(number string) bool {
+	// Assuming extension numbers are purely numeric and 4-6 digits long
+	matched, _ := regexp.MatchString(`^\d{4,6}$`, number)
+	return matched
+}
